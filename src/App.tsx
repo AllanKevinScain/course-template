7aeb09eb34be16e1891e0ee409586f96008891e6
AllanKevinScain <meuemail44allan@gmail.com>
2022-02-18 08:48:05 -0300

    config plugin typescrypt and Stitches

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./styles/components";
+
+export const App: React.FC = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#e5e5e5",
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Button>Ola mundo</Button>
+    </div>
+  );
+};
